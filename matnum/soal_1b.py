29fc25c8be457bb5492384a9703e99d7f72141f5
Tito Darmawan <89248681+Linaeus14@users.noreply.github.com>
2022-05-18 18:56:33 +0800

    Update soal_1b.py
    Punya Herni

--- matnum/soal_1b.py
+++ matnum/soal_1b.py
@@ -1,49 +1,46 @@
-x1 = int(input("x1: "))
-y1 = int(input("y1: "))
-hasil1 = int(input("hasil: "))
-print("==========")
-x2 = int(input("x2: "))
-y2 = int(input("y2: "))
-hasil2 = int(input("hasil: "))
-if x1 > x2:
-    a = x1/x2
-    X2 = a * x2
-    Y2 = a * y2
-    hasil3 = a * hasil2
-    x = x1-X2
-    y = y1-Y2
-    hasil = hasil1-hasil3
-    hasil = hasil/y
-    Y = hasil
-else:
-    a = x2/x1
-    X1 = a * x1
-    Y1 = a * y1
-    hasil3 = a * hasil1
-    x = x2-X1
-    y = y2-Y1
-    hasil = hasil2-hasil3
-    hasil = hasil/y
-    Y = hasil
-if y1 > y2:
-    a = y1/y2
-    X2 = a * x2
-    Y2 = a * y2
-    hasil3 = a * hasil2
-    x = x1-X2
-    y = y1-Y2
-    hasil = hasil1-hasil3
-    hasil = hasil/x
-    X = hasil
-else:
-    a = y2/y1
-    X1 = a * x1
-    Y1 = a * y1
-    hasil3 = a * hasil1
-    x = x2-X1
-    y = y2-Y1
-    hasil = hasil2-hasil3
-    hasil = hasil/x
-    X = hasil
-print("==========")
-print(f"Hasil penyelesaian dari {x1}x + {y1}y = {hasil1} dan {x2}x + {y2}y = {hasil2} adalah x = {X} dan y = {Y}")
+# Fungsi untuk metode eliminasi
+def eliminasi(x1,y1,c1,x2,y2,c2):
+    print ("\nPENYELESAIAN DENGAN METODE ELIMINASI")
+    print(f"Persamaan 1  {x1}x + {y1}y = {c1}")
+    print(f"Persamaan 2  {x2}x + {y2}y = {c2}")
+    print("----------------------------------")
+    print("Proses eliminasi mencari nilai Y")
+    x3= x2*x1
+    y3= y1*x2
+    c3= c1*x2
+    x4= x1*x2
+    y4= y2*x1
+    c4= c2*x1
+    print(f"{x1}x + {y1}y = {c1} | {x2} | {x3}x + {y3}y = {c3}")
+    print(f"{x2}x + {y2}y = {c2} | {x1} | {x4}x + {y4}y = {c4}")
+    print("----------------------------------")
+    print(f"{x3}x + {y3}y = {c3}")
+    print(f"{x4}x + {y4}y = {c4}")
+    print("-------------------------------- -")
+    proses1_1= y3-y4
+    proses1_2= c3-c4
+    print(f"({x3}x - {x4}x) + ({y3}y - {y4}y) = ({c3} - {c4})")
+    print(f"{proses1_1}y = {proses1_2}")
+    y= proses1_2/proses1_1
+    print(f"y = {proses1_2} / {proses1_1}")
+    print(f"y = {y}")
+    print("\nProses eliminasi mencari nilai X")
+    x3= x1*y2
+    y3= y1*y2
+    c3= c1*y2
+    x4= x2*y1
+    y4= y2*y1
+    c4= c2*y1
+    print(f"{x1}x + {y1}y = {c1} | {y2} | {x3}x + {y3}y = {c3}")
+    print(f"{x2}x + {y2}y = {c2} | {y1} | {x4}x + {y4}y = {c4}")
+    print("----------------------------------")
+    print(f"{x3}x + {y3}y = {c3}")
+    print(f"{x4}x + {y4}y = {c4}")
+    print("-------------------------------- -")
+    proses1_1= x3-x4
+    proses1_2= c3-c4
+    print(f"({x3}x - {x4}x) + ({y3}y - {y4}y) = ({c3} - {c4})")
+    print(f"{proses1_1}x = {proses1_2}")
+    x= proses1_2/proses1_1
+    print(f"x = {proses1_2} / {proses1_1}")
+    print(f"x = {x}")
