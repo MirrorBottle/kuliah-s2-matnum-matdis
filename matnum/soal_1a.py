7bf344272266a0f8280c1ab2469fdb66f7506f3b
Tito Darmawan <89248681+Linaeus14@users.noreply.github.com>
2022-05-18 18:50:57 +0800

    Update soal_1a.py
    Punya herni

--- matnum/soal_1a.py
+++ matnum/soal_1a.py
@@ -1,38 +1,36 @@
-a=3
-b=5
-c=5
-d=2
-total1=44
-total2=29
-print("------------------------")
-print("-- Metode Subtitusi --")
-print("------------------------")
-print(f"{a}x + {b}y = {total1}")
-print(f"{c}x + {d}y = {total2}")
-print("------------------------")
-print(f"{c}x + {d}y = {total2}")
-print(f"{c}x = {total2} - {d}y")
-dbagic = d / c
-total2bagic = total2 / c
-print(f"x = {total2bagic} - {dbagic}y")
-print("------------------------")
-print(f"{a}x + {b}y = {total1}")
-print(f"{a}({total2bagic} - {dbagic}y) + {b}y = {total1}")
-ac = total2bagic * a
-adc = a * dbagic
-print(f"{ac} - {adc}y + {b}y = {total1}")
-print(f"-{adc}y + {b}y = {total1} - {ac}")
-total1kurangac = total1 - ac
-adckurangb = -adc + b
-print(f"{adckurangb}y = {total1kurangac}")
-ketemu_y = total1kurangac / adckurangb
-print(f"y = {ketemu_y}")
-print("------------------------")
-print(f"x = {total2bagic} - {dbagic}y")
-print(f"x = {total2bagic} - {dbagic}({ketemu_y})")
-dcy = dbagic * ketemu_y
-print(f"x = {total2bagic} - {dcy}")
-ketemu_x = total2bagic - dcy
-print(f"x = {ketemu_x}")
-print("------------------------")
-print(f"Jadi, x = {ketemu_x} dan y = {ketemu_y}")
+def subtitusi(x1,y1,c1,x2,y2,c2):
+    print ("\nPENYELESAIAN DENGAN METODE SUBSTITUSI")
+    print(f"Persamaan 1  {x1}x + {y1}y = {c1}")
+    print(f"Persamaan 2  {x2}x + {y2}y = {c2}")
+    print("----------------------------------")
+    print(f"{x1}x = {c1} - {y1}y")
+    print(f" x = ({c1} - {y1}y) / {x1}")
+    print("-----------------------------------")
+    print("Mencari nilai y")
+    print(f"{x2}x + {y2}y = {c2}")
+    print(f"{x2} * (({c1} - {y1}y)/{x1}) + {y2}y = {c2}")
+    proses1_1= c1*x2
+    proses1_2= y1*x2
+    print(f"({proses1_1} - {proses1_2}y)/{x1} + {y2}y = {c2}")
+    print(f" ------------------------- x {x1}")
+    proses2_1= y2*x1
+    proses2_2= c2*x1
+    print(f"{proses1_1} - {proses1_2}y + {proses2_1}y = {proses2_2}")
+    print(f"- {proses1_2}y + {proses2_1}y = {proses2_2} - {proses1_1}")
+    proses3_1= (-proses1_2)+proses2_1
+    proses3_2= proses2_2-proses1_1
+    print(f"{proses3_1}y = {proses3_2}")
+    print(f"y = {proses3_2} / {proses3_1}")
+    y= proses3_2/proses3_1
+    print(f"y = {y}")
+    print("-----------------------------------")
+    print ("Mencari nilai x")
+    print(f"{x1}x + {y1}y = {c1}")
+    proses4= y1*y
+    print(f"{x1}x + {proses4} = {c1}")
+    print(f"{x1}x = {c1} - {proses4}")
+    proses5= c1-proses4
+    print(f"{x1}x = {proses5}")
+    print(f"x = {proses5} / {x1}")
+    x= proses5/x1
+    print(f"x = {x}")
